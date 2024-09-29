674aa3d28c3778346e3a2e22d10d122cd3f9d613
Lacri <nov@lacrivoca.net>
2024-09-29 12:52:59 +0800

    Update to add config

---
+++ DeathQuota/ConfigMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using BepInEx;
+using BepInEx.Configuration;
+using CSync.Extensions;
+using CSync.Lib;
+
+namespace DeathQuota
+{
+    public class ConfigMain : SyncedConfig2<ConfigMain>
+    {
+        [DataMember] public SyncedEntry<bool> ReplaceVanilla {  get; private set; }
+        [DataMember] public SyncedEntry<int> BodyCost { get; private set; }
+        public ConfigMain(ConfigFile cfg) : base("lacrivoca.DeathQuota"){
+            ConfigManager.Register(this);
+            ReplaceVanilla = cfg.BindSyncedEntry(
+                new ConfigDefinition("General", "Replace Vanilla"),
+                false,
+                new ConfigDescription("Whether to completely replace the vanilla quota system with a 20% increase per unrecovered body or to simply add a flat amount per body.")
+                );
+
+            BodyCost = cfg.BindSyncedEntry(
+                new ConfigDefinition("General", "Body Cost"),
+                50,
+                new ConfigDescription("Amount to increase the quota by per unrecovered body. Only used if Replace Vanilla is set to false.")
+                );
+        }
+    }
+}
